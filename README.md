78f92226e950582869764b72eed28b035bb5947b
iamsankalp <mcs2019009@iiitl.ac.in>
2021-05-13 20:31:39 +0530

    done

---
+++ README.md
@@ -0,0 +1,6 @@
+# Hydrate
+A chrome extension which reminds user every hour to get hydrated and fix their posture during long hours of Work From Home.
+
+## Controls
+Automatically starts whenever chrome gets started.
+Click on the extension to toggle on/off manually (alert will popup for on/off trigger).
--- hydrate/background.js
+++ hydrate/background.js
@@ -4,31 +4,31 @@ const toggle = {
 const switchToggle = function setToggleFalse(toggle){
     if (toggle.status === true){
         toggle.status = false
+        console.log("Stopped extension")
         alert("Stopped Hydrate!\nClick again to restart")
     }
     else{
         toggle.status = true
+        console.log("Restarted extension")
         alert("Restarted Hydrate!\nClick again to stop")
         notification()
     }    
 }
-console.log("script started ")
+console.log("Extension started ")
 console.log("Developed by Sankalp Sharma (http://www.sharmasankalp.com)")
 const delay = ms => new Promise(res => setTimeout(res, ms))
-const myDelay = async () => {
+const myDelay = async (interval) => {
     console.log("waited!")
-    await delay(3600000)
+    await delay(interval)
     console.log("waited!")
     return
   }
 
 const notification = async()=>{
     const interval = 3600000
-    let timeLeft = 24*3600*1000//24hr for safe keeping
-    while (timeLeft>0 && toggle.status)
+    while (toggle.status)
     {   
-        await myDelay(3600000)
-        timeLeft-=interval
+        await myDelay(interval)
         if (toggle.status == false){
             break
         }
