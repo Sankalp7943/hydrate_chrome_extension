c5db298cd6bc69b259c2ab52a749bde8d60fdd2c
Sankalp Sharma <sharma.sankalp97@gmail.com>
2024-12-14 09:13:48 +0530

    moved to manifest v3

--- hydrate/background.js
+++ hydrate/background.js
@@ -1,10 +1,10 @@
 const toggle = {
     status: true
-}
+};
 
 const switchToggle = function setToggleFalse(toggle) {
     if (toggle.status === true) {
-        chrome.browserAction.setIcon({
+        chrome.action.setIcon({
             path: {
                 16: "icons/icon-gray16.png",
                 32: "icons/icon-gray32.png",
@@ -12,11 +12,16 @@ const switchToggle = function setToggleFalse(toggle) {
                 128: "icons/icon-gray128.png"
             }
         });
-        toggle.status = false
-        console.log("Stopped extension")
-        alert("Stopped Hydrate!\nClick again to restart")
+        toggle.status = false;
+        console.log("Stopped extension");
+        chrome.notifications.create({
+            type: "basic",
+            iconUrl: "icons/icon-gray48.png",
+            title: "Hydrate Stopped",
+            message: "Stopped Hydrate! Click the icon again to restart."
+        });
     } else {
-        chrome.browserAction.setIcon({
+        chrome.action.setIcon({
             path: {
                 16: "icons/icon16.png",
                 32: "icons/icon32.png",
@@ -24,36 +29,46 @@ const switchToggle = function setToggleFalse(toggle) {
                 128: "icons/icon128.png"
             }
         });
-        toggle.status = true
-        console.log("Restarted extension")
-        alert("Restarted Hydrate!\nClick again to stop")
-        notification()
+        toggle.status = true;
+        console.log("Restarted extension");
+        chrome.notifications.create({
+            type: "basic",
+            iconUrl: "icons/icon48.png",
+            title: "Hydrate Restarted",
+            message: "Restarted Hydrate! Click the icon again to stop."
+        });
+        notification();
     }
-}
+};
 
-console.log("Extension started ")
-console.log("Developed by Sankalp Sharma (http://www.sharmasankalp.com)")
-const delay = ms => new Promise(res => setTimeout(res, ms))
+console.log("Extension started");
+console.log("Developed by Sankalp Sharma (http://www.sharmasankalp.com)");
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
 const myDelay = async (interval) => {
-    console.log("waiting!")
-    await delay(interval)
-    console.log("waiting!")
-    return
-}
+    console.log("waiting!");
+    await delay(interval);
+    console.log("waiting!");
+    return;
+};
 
 const notification = async () => {
-    const interval = 3600000
+    const interval = 3600000; // 1 hour
     while (toggle.status) {
-        await myDelay(interval)
-        if (toggle.status == false) {
-            break
+        await myDelay(interval);
+        if (!toggle.status) {
+            break;
         }
-        alert("Hey! Time to drink some water, stretch your back and rest your eyes.\nRemember to keep your back straight.")
+        chrome.notifications.create({
+            type: "basic",
+            iconUrl: "icons/icon48.png",
+            title: "Stay Hydrated!",
+            message: "Time to drink some water, stretch your back, and rest your eyes."
+        });
     }
-}
+};
 
-notification()
-
-chrome.browserAction.onClicked.addListener(function (tab) {
-    switchToggle(toggle)
-})
+// Add a listener for action clicks
+chrome.action.onClicked.addListener(() => {
+    switchToggle(toggle);
+});
